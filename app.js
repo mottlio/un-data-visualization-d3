49381434509088d11558c4ee04a30a0f7e8b5943
Michal Mottl <michal.mottl@coleurope.eu>
2018-05-12 13:54:45 +0700

    add title

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var width = 600;
 var height = 600;
-var padding = 30;
+var padding = 80;
 
 
 var yMax = d3.max(regionData, d => d.subscribersPer100);
@@ -33,3 +33,13 @@ d3.select("svg")
     .attr("cy", d => yScale(d.subscribersPer100))
     .attr("fill", d => colorScale(d.extremePovertyRate))
     .attr("r", d => radiusScale(d.medianAge));
+
+//adding title, X and Y axis labels
+
+d3.select("svg")
+  .append("text")
+    .attr("x", width/2)
+    .attr("y", 50)
+    .style("text-anchor", "middle")
+    .style("font-size", "1.2em")
+    .text("Mobile subscriptions and urban population rate")
